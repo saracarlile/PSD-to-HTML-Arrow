b1d43898f6d4ecb21725d378fe55282aa81c659e
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2018-02-14 10:23:25 -0600

    our clients section

--- js/main.js
+++ js/main.js
@@ -1,41 +1,35 @@
 $(document).ready(function () {
 
-    new WOW().init();
 
-    $('.js-wp-2').waypoint(function (direction) {
-
-        $('.js-wp-2').addClass('animated slideInUp');
-
-    }, {
-            offset: '70%'
+        $('.flex-container-clients div').on('click', function(e) {  
+            $(this).addClass('triangle-logo').siblings().removeClass('triangle-logo');  //add triangle to bottom of client img when clicked
+           
+            if (jQuery(this).attr('id') == 'google') {
+                $('.review-text').text('Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.');
+                $('.review-person').text('Sundar Pichai, Google.com');
+            }
+            else if (jQuery(this).attr('id') == 'booking') {
+                $('.review-text').text('Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. ');
+                $('.review-person').text('Gillian Tans, Booking.com');
+            }
+            else if (jQuery(this).attr('id') == 'dropbox') {
+                $('.review-text').text('The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.');
+                $('.review-person').text('Drew Houston, Dropbox.com');
+            }
+            else if (jQuery(this).attr('id') == 'facebook') {
+                $('.review-text').text('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
+                $('.review-person').text('Mark Zuckerberg, Facebook.com');
+            }
+            else if (jQuery(this).attr('id') == 'groupon') {
+                $('.review-text').text('It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.');
+                $('.review-person').text('Rich Williams, Groupn.com');
+            }
+            else if (jQuery(this).attr('id') == 'paypal') {
+                $('.review-text').text('Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.');
+                $('.review-person').text('Daniel Schulman, Paypal.com');
+            }
         });
 
-    $('.js-wp-3').waypoint(function (direction) {
-
-        $('.js-wp-3').addClass('animated fadeIn');
-
-    }, {
-            offset: '70%'
-        });
-
-    $('.js-wp-4').waypoint(function (direction) {
-
-        $('.js-wp-4').addClass('animated fadeIn');
-
-    }, {
-            offset: '30%'
-        });
-
-    /* had to change instructor's code here to use setTimeout instead of just using JQuery's delay function to get animation working */
-
-    setTimeout(function () { $('.iphone-btn').animate({ bottom: "+=-3" }, 300).animate({ top: "+=-3" }, 300).delay(800) }, 1500);
-
-    
-    $('.burger-nav').on('click', function(){
-        $('.container nav ul').toggleClass('open');
-    });
-
-
-
+        
 
 });
